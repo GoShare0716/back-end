d8534e20e69afa21dc5dc85eb570867223c7e86b
yun_cloud <ookk8282@gmail.com>
2017-07-20 18:36:15 +0800

    fake data

---
+++ src/data/skill.js
@@ -0,0 +1,32 @@
+var datas = [
+  {
+    id: 1,
+    name: 'Javascript',
+    description: 'JavaScript (JS) is a programming language mostly used client-side to dynamically script webpages, but often also server-side.',
+    tag: ['程式語言', 'Web'],
+    videoUrl: 'https://www.youtube.com/watch?v=Ukg_U3CnJWI',
+    visible: true,
+    createdAt: 1500541846538,
+    updatedAt: 1500541846538
+  }, {
+    id: 2,
+    name: '平面設計',
+    description: '泛指具有藝術性和專業性，以「視覺」作為溝通和表現的方式。透過多種方式來創造和結合符號、圖片和文字，藉此作出用來傳達想法或訊息的視覺表現。',
+    tag: ['Design'],
+    videoUrl: 'https://www.youtube.com/watch?v=80kGfeEAML4',
+    visible: true,
+    createdAt: 1500542167429,
+    updatedAt: 1500542167429
+  }, {
+    id: 3,
+    name: 'Django',
+    description: 'Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design.',
+    tag: ['Web', 'Full Stack'],
+    videoUrl: 'https://www.youtube.com/watch?v=FNQxxpM1yOs',
+    visible: true,
+    createdAt: 1500542493618,
+    updatedAt: 1500542493618
+  }
+]
+
+module.exports = datas
