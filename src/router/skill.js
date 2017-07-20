f0925b1cc966a854afedfbbcdd90fb84012ca96b
yun_cloud <ookk8282@gmail.com>
2017-07-20 18:35:48 +0800

    Api skill finished

--- src/router/skill.js
+++ src/router/skill.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const skillDatas = require('../data/skill.js')
+const userDatas = require('../data/user.js')
 
 const router = express.Router()
 
@@ -13,18 +15,48 @@ router.post(`${baseUrl}`, (req, res, next) => {
 
 // List {{{1
 router.get(`${baseUrl}`, (req, res, next) => {
-
+  var skills = skillDatas.slice()
+  var basicNumbers = [3, 7, 6]
+  var advancedNumbers = [5, 1, 2]
+  var friendLists = [
+    [1],
+    [2, 3],
+    []
+  ]
+  for (let i = 0; i < 3; i++) {
+    friendLists[i] = friendLists[i].map(friendThumbnail)
+  }
+  for (let i = 0; i < 3; i++) {
+    skills[i].basicNumber = basicNumbers[i]
+    skills[i].advancedNumber = advancedNumbers[i]
+    skills[i].friends = friendLists[i]
+  }
+  res.json(skills)
 })
 
 // View {{{1
 router.get(`${baseUrl}/:id`, (req, res, next) => {
-
+  var skill = skillDatas[0]
+  skill.basicNumber = 3
+  skill.advancedNumber = 5
+  skill.friends = [1].map(friendThumbnail)
+  res.json(skill)
 })
 
 // Update {{{1
 router.put(`${baseUrl}/:id`, (req, res, next) => {
-
+  var skill = skillDatas[0]
+  skill.updatedAt = Date.now()
+  res.json(skill)
 })
+
+// util functions {{{1
+function friendThumbnail (id) {
+  return {
+    id,
+    thumbnailUrl: userDatas[id - 1].thumbnailUrl
+  }
+}
 // }}}
 
 module.exports = router
