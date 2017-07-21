4302ddc3066481eff4d70a3f8bb4843f8630e6c9
USER <ookk8282@gmail.com>
2017-07-21 18:15:16 +0800

    Change dir and modify a little

---
+++ src/table/other.js
@@ -0,0 +1,9 @@
+const friends = [
+  [1],
+  [2, 3],
+  []
+]
+
+module.exports = {
+  friends
+}
--- src/table/skill.js
+++ src/table/skill.js
@@ -2,6 +2,7 @@ var datas = [
   {
     id: 1,
     name: 'Javascript',
+    category: 'technology',
     description: 'JavaScript (JS) is a programming language mostly used client-side to dynamically script webpages, but often also server-side.',
     tag: ['程式語言', 'Web'],
     videoUrl: 'https://www.youtube.com/watch?v=Ukg_U3CnJWI',
@@ -11,6 +12,7 @@ var datas = [
   }, {
     id: 2,
     name: '平面設計',
+    category: 'aesthtics',
     description: '泛指具有藝術性和專業性，以「視覺」作為溝通和表現的方式。透過多種方式來創造和結合符號、圖片和文字，藉此作出用來傳達想法或訊息的視覺表現。',
     tag: ['Design'],
     videoUrl: 'https://www.youtube.com/watch?v=80kGfeEAML4',
@@ -20,6 +22,7 @@ var datas = [
   }, {
     id: 3,
     name: 'Django',
+    category: 'technology',
     description: 'Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design.',
     tag: ['Web', 'Full Stack'],
     videoUrl: 'https://www.youtube.com/watch?v=FNQxxpM1yOs',
