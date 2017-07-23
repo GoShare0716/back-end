70cc7df986d3e1a4f96a0f295ec632d87068de66
USER <ookk8282@gmail.com>
2017-07-23 15:57:16 +0800

    Finish User - View API

--- src/router/user.js
+++ src/router/user.js
@@ -1,12 +1,50 @@
 const express = require('express')
 
+const userTable = require('../table/user.js')
+const skillTable = require('../table/skill.js')
+const workshopTable = require('../table/workshop.js')
+const {
+  voteSkillTable,
+  equipSkillTable,
+  createWorkshopTable,
+  attendWorkshopTable
+} = require('../table/foreign.js')
+
 const router = express.Router()
 
 const baseUrl = '/users'
 
 // View {{{1
 router.get(baseUrl + '/:id', (req, res, next) => {
+  const userId = +req.params.id
+  const belongToMe = item => item.userId === userId
 
+  const voteSkills = voteSkillTable
+    .filter(belongToMe)
+    .map(vote => skillTable[vote.skillId - 1])
+    .filter(skill => skill.visible)
+
+  const equipSkills = equipSkillTable
+    .filter(belongToMe)
+    .map(equip => skillTable[equip.skillId - 1])
+
+  const createWorkshops = createWorkshopTable
+    .filter(belongToMe)
+    .map(create => workshopTable[create.workshopId - 1])
+
+  const attendWorkshops = attendWorkshopTable
+    .filter(belongToMe)
+    .map(attend => workshopTable[attend.workshopId - 1])
+
+  res.json(Object.assign({},
+    userTable[userId - 1],
+    {
+      voteSkills,
+      equipSkills,
+      createWorkshops,
+      attendWorkshops
+    }
+  ))
 })
 
 // Update Email {{{1
