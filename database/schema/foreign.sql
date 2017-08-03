5e382c5b3bdf32da8ab0625fc38dff61c6253947
USER <ookk8282@gmail.com>
2017-08-03 21:33:01 +0800

    (schema)[foreign] All with create_at field now

--- database/schema/foreign.sql
+++ database/schema/foreign.sql
@@ -3,28 +3,33 @@
 -- that are referenced to the rows that are being deleted in the origin table
 CREATE TABLE create_workshop (
     user_id     serial REFERENCES users(id)    ON DELETE CASCADE,
-    workshop_id serial REFERENCES workshop(id) ON DELETE CASCADE
+    workshop_id serial REFERENCES workshop(id) ON DELETE CASCADE,
+    created_at  bigint  NOT NULL DEFAULT (extract(epoch from now())*1000)
 );
 
 CREATE TABLE attend_workshop (
     user_id     serial REFERENCES users(id)    ON DELETE CASCADE,
     workshop_id serial REFERENCES workshop(id) ON DELETE CASCADE,
+    created_at  bigint  NOT NULL DEFAULT (extract(epoch from now())*1000),
     canceled    BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE create_skill (
-    user_id  serial REFERENCES users(id) ON DELETE CASCADE,
-    skill_id serial REFERENCES skill(id) ON DELETE CASCADE
+    user_id    serial REFERENCES users(id) ON DELETE CASCADE,
+    skill_id   serial REFERENCES skill(id) ON DELETE CASCADE,
+    created_at bigint NOT NULL DEFAULT (extract(epoch from now())*1000)
 );
 
 CREATE TABLE vote_skill (
-    user_id  serial REFERENCES users(id) ON DELETE CASCADE,
-    skill_id serial REFERENCES skill(id) ON DELETE CASCADE,
-    level    level  NOT NULL
+    user_id    serial REFERENCES users(id) ON DELETE CASCADE,
+    skill_id   serial REFERENCES skill(id) ON DELETE CASCADE,
+    created_at bigint NOT NULL DEFAULT (extract(epoch from now())*1000),
+    level      level  NOT NULL
 );
 
 CREATE TABLE equip_skill (
-    user_id  serial REFERENCES users(id) ON DELETE CASCADE,
-    skill_id serial REFERENCES skill(id) ON DELETE CASCADE,
-    level    level  NOT NULL
+    user_id    serial REFERENCES users(id) ON DELETE CASCADE,
+    skill_id   serial REFERENCES skill(id) ON DELETE CASCADE,
+    created_at bigint NOT NULL DEFAULT (extract(epoch from now())*1000),
+    level      level  NOT NULL
 )
