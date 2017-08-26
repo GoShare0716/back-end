0071075d669ab86aac1bce2b4367f6eb57940bd8
USER <ookk8282@gmail.com>
2017-08-26 21:12:56 +0800

    (API)[user-view] just like workshop-list

--- src/sql/user/attend-workshops-public.sql
+++ src/sql/user/attend-workshops-public.sql
@@ -1,9 +1,34 @@
 SELECT
-    workshop.*
-FROM attend_workshop
-INNER JOIN workshop
-ON attend_workshop.workshop_id = workshop.id
+  w.id,
+  w.state,
+  w.image_url,
+  w.title,
+  w.min_number,
+  w.max_number,
+  w.deadline,
+  w.closing,
+  w.start_datetime,
+  w.pre_price,
+  w.price,
+  w.published, --- filter usage, not for api
+  COUNT(a.user_id)::integer AS attendees_number,
+  u.id               AS author_id,
+  u.name             AS author_name,
+  u.picture_url      AS author_picture_url,
+  u.fb_url           AS author_fb_url,
+  u.personal_web_url AS author_personal_web_url,
+  u.introduction     AS author_introduction
+FROM workshop AS w
+LEFT JOIN attend_workshop AS a
+ON a.workshop_id = w.id AND a.canceled = false
+INNER JOIN create_workshop AS c
+ON c.workshop_id = w.id
+INNER JOIN users AS u
+ON u.id = c.user_id
 WHERE
-    attend_workshop.user_id = $(userId) AND
-    workshop.published = true AND
-    workshop.state NOT IN ('judging', 'judge_na');
+    a.user_id = ${userId} AND
+    w.published = true AND
+    w.state NOT IN ('judging', 'judge_na')
+GROUP BY
+  w.id,
+  u.id;
--- src/sql/user/create-workshops-all.sql
+++ src/sql/user/create-workshops-all.sql
@@ -1,7 +1,32 @@
 SELECT
-    workshop.*
-FROM create_workshop
-INNER JOIN workshop
-ON create_workshop.workshop_id = workshop.id
+  w.id,
+  w.state,
+  w.image_url,
+  w.title,
+  w.min_number,
+  w.max_number,
+  w.deadline,
+  w.closing,
+  w.start_datetime,
+  w.pre_price,
+  w.price,
+  w.published, --- filter usage, not for api
+  COUNT(a.user_id)::integer AS attendees_number,
+  u.id               AS author_id,
+  u.name             AS author_name,
+  u.picture_url      AS author_picture_url,
+  u.fb_url           AS author_fb_url,
+  u.personal_web_url AS author_personal_web_url,
+  u.introduction     AS author_introduction
+FROM workshop AS w
+LEFT JOIN attend_workshop AS a
+ON a.workshop_id = w.id AND a.canceled = false
+INNER JOIN create_workshop AS c
+ON c.workshop_id = w.id
+INNER JOIN users AS u
+ON u.id = c.user_id
 WHERE
-    create_workshop.user_id = $(userId);
+    c.user_id = ${userId}
+GROUP BY
+  w.id,
+  u.id;
