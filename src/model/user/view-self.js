9ee2f7e73dd65d8e66e4a4f00898888c50e95d3c
USER <ookk8282@gmail.com>
2017-08-26 21:26:22 +0800

    organize and add phase on workshop [user-view, view-self]

--- src/model/user/view-self.js
+++ src/model/user/view-self.js
@@ -2,14 +2,29 @@ const R = require('ramda')
 
 const db = require('src/db')
 const sql = require('src/sql').user
+const utils = require('src/utils')
 
 module.exports = (userId) => {
+  const now = Date.now()
+
   return db.task(t => {
     // pairs :: (PropName, Sql)[]
     const pairs = [
       ['profile', () => t.one(sql.profile, {userId})],
-      ['createWorkshops', () => t.any(sql.createWorkshopsAll, {userId})],
-      ['attendWorkshops', () => t.any(sql.attendWorkshopsPublic, {userId})]
+      ['createWorkshops', () => {
+        return t.any(sql.createWorkshopsAll, {userId})
+          .map(utils.workshop.assocPhase(now))
+          .map(R.assoc('friends', [])) // TODO temp
+          .map(utils.organize(['author', 'attendees']))
+          .map(utils.workshop.adapter)
+      }],
+      ['attendWorkshops', () => {
+        return t.any(sql.attendWorkshopsPublic, {userId})
+          .map(utils.workshop.assocPhase(now))
+          .map(R.assoc('friends', [])) // TODO temp
+          .map(utils.organize(['author', 'attendees']))
+          .map(utils.workshop.adapter)
+      }]
     ]
     const props = pairs.map(R.nth(0))
     const sqls = pairs.map(R.nth(1))
