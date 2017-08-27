1441f1ac182e6db1d6f187ba826ebf81d0e8f255
USER <ookk8282@gmail.com>
2017-08-27 22:50:25 +0800

    (shell) migrate local db's schema to rds

--- shell/dump.sh
+++
@@ -1,5 +0,0 @@
-DUMP_FILE=db.dump
-
-echo "dump local database ${PG_DB_NAME}..."
-pg_dump -h ${PG_HOSTNAME} -U ${PG_USERNAME} --no-owner -c ${$PG_DB_NAME} > ${DUMP_FILE}
-echo "dump done. (${DUMP_FILE})"
--- shell/migrate-to-rds.sh
+++ shell/migrate-to-rds.sh
@@ -1,4 +1,9 @@
-DUMP_FILE='db.dump'
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DUMP_FILE="${BASEDIR}/db.dump"
+echo "DUMP_FILE = ${DUMP_FILE}"
 
-echo "migrate ${DUMP_FLIE} to ${RDS_DB_NAME}..."
+echo "dumping schema of local database ${PG_DB_NAME}..."
+pg_dump -h ${PG_HOSTNAME} -U ${PG_USERNAME} --no-owner --schema-only -c ${PG_DB_NAME} > ${DUMP_FILE}
+echo "migrate to ${RDS_DB_NAME}..."
 psql -h ${RDS_HOSTNAME} -U ${RDS_USERNAME} ${RDS_DB_NAME} < ${DUMP_FILE}
+rm ${DUMP_FILE}
