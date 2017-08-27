d136bce0268220d56ad09f433cd6adfed84d6b79
USER <ookk8282@gmail.com>
2017-08-27 22:46:55 +0800

    (npm) init-db-schema-only command

--- database/init-db-schema-only.js
+++ database/init-db-schema-only.js
@@ -7,15 +7,9 @@ require('app-module-path').addPath(path.join(__dirname, '../'))
 const db = require('src/db')
 
 const schemaSql = require('./schema')
-const dataSql = require('./data')
 
 db.none(schemaSql).then(() => {
   console.log('Schema created')
-  return db.none(dataSql).then(() => {
-    console.log('Data populated')
-  }).catch(err => {
-    console.log('Error populated data', err)
-  })
 }).catch(err => {
   console.log('Error creating schema', err)
 }).finally(() => {
