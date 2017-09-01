9391d157973c35adb738e5e478d5236c25e94be6
USER <ookk8282@gmail.com>
2017-09-01 20:45:51 +0800

    Fixed show friends break guest user

--- src/utils/facebook.js
+++ src/utils/facebook.js
@@ -1,5 +1,6 @@
 const R = require('ramda')
 const FB = require('fb')
+const Promise = require('bluebird')
 
 const log = x => {
   console.log(JSON.stringify(x, null, 2))
@@ -8,8 +9,12 @@ const log = x => {
 
 function friends (user) {
   // TODO handle pagination
+  if (user.id === -1) {
+    return Promise.resolve(['-1'])
+  }
   return FB.api('/me/friends', { access_token: user.accessToken })
     .then(res => res.data.map(R.prop('id')))
+    .then(log)
 }
 
 module.exports = {
