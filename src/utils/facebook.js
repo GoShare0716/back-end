5f1fdd88fcdb09f37fac4d721617e8b619c8486b
USER <ookk8282@gmail.com>
2017-09-02 22:09:57 +0800

    Fix when no friends, sql error

--- src/utils/facebook.js
+++ src/utils/facebook.js
@@ -2,19 +2,18 @@ const R = require('ramda')
 const FB = require('fb')
 const Promise = require('bluebird')
 
-const log = x => {
-  console.log(JSON.stringify(x, null, 2))
-  return x
-}
-
 function friends (user) {
   // TODO handle pagination
   if (user.id === -1) {
     return Promise.resolve(['-1'])
   }
   return FB.api('/me/friends', { access_token: user.accessToken })
-    .then(res => res.data.map(R.prop('id')))
-    .then(log)
+    .then(R.prop('data'))
+    .then(R.ifElse(
+      R.isEmpty,
+      R.always(Promise.resolve(['-1'])),
+      R.map(R.prop('id'))
+    ))
 }
 
 module.exports = {
