f59943ba2303143cf557ea2160cc20896b8970e9
USER <ookk8282@gmail.com>
2017-09-06 09:48:45 +0800

    (data) Change user's fb_id to GoShare app

--- database/data/users.js
+++ database/data/users.js
@@ -8,8 +8,8 @@ const columnSet = new pgp.helpers.ColumnSet(
     'name',
     'role',
     'email',
-    // 'fb_id',
-    // 'access_token',
+    'fb_id',
+    'access_token',
     'thumbnail_url',
     'picture_url',
     'introduction',
@@ -31,7 +31,7 @@ var datas = [
     name: '_張嘉軒',
     role: 'member',
     email: 'ookk8282@gmail.com',
-    // fb_id: '1514864711922034',
+    fb_id: '1662384910503346',
     thumbnail_url: 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-1/p40x40/16426235_1335337256541448_4112461475677668738_n.jpg?oh=44bcbeb78e0f146ae8a22b56e20fd444&oe=5A0AD7C3',
     picture_url: 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-1/p240x240/16426235_1335337256541448_4112461475677668738_n.jpg?oh=ad639f0d623029c602a74d6a54bb0d35&oe=5A0E3AB1',
     introduction: 'intro',
@@ -42,7 +42,7 @@ var datas = [
     role: 'admin',
     // role: 'member',
     email: 'skyle0115@gmail.com',
-    // fb_id: '1833867746937550',
+    fb_id: '1909451312712526',
     thumbnail_url: 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-1/c0.31.80.80/p80x80/18622427_1859238271067164_3869120362467491071_n.jpg?oh=2c4879f3c50e4ed99de714439435ce08&oe=59F71BC3',
     picture_url: 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-1/c0.93.240.240/p240x240/18622427_1859238271067164_3869120362467491071_n.jpg?oh=657cea4638763d6ebb3fc90ed90ddc4f&oe=5A02C22A',
     introduction: 'intro',
@@ -52,7 +52,7 @@ var datas = [
     name: '_林湘庭',
     role: 'member',
     email: 'empty',
-    // fb_id: '1194279457367537',
+    fb_id: '123', // TODO
     thumbnail_url: 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-1/p80x80/14915540_1715220865464246_4727687316731100143_n.jpg?oh=ad9590fd0ee9b970cf82e2cd35bfb70d&oe=59C4DCB2',
     picture_url: 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-1/p240x240/14915540_1715220865464246_4727687316731100143_n.jpg?oh=9527e4dbccd5eeb02dbba706ba64de92&oe=5A10AA18',
     introduction: 'intro',
@@ -77,9 +77,9 @@ function genAvai () {
   return arr
 }
 
-// for (let x of datas) {
-//   x.access_token = process.env[`ACCESS_TOKEN_${x.fb_id}`]
-// }
+for (let x of datas) {
+  x.access_token = process.env[`ACCESS_TOKEN_${x.fb_id}`]
+}
 
 module.exports = {
   columnSet,
